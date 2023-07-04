194d62355d7dccedea4e767ebf05a1d751ee7a3e
AndreSoftwareDeveloper <andriej2301@gmail.com>
2023-07-04 16:55:26 +0200

    Initial commit.

---
+++ Wielowątkowość/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Wielowątkowość
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Compute prime numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(358, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Compute powers of two";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(487, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Get current Unix time";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Multithreading Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Wielowątkowość/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Wielowątkowość
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }  
+
+        private int CalculatePrimeNumbers()
+        {
+            String currentPrimeNumber = label1.Text;
+            for (int i = 1; true; i++)
+            {
+                if (i > Convert.ToInt32(currentPrimeNumber))
+                {
+                    if (IsPrime(i))
+                    {
+                        label1.Text = i.ToString();
+                        Thread.Sleep(1000);
+                    }
+                }                                                           
+            }
+        }
+
+        private void CalculatePowersOfTwo()
+        {
+            int currentPower = Convert.ToInt32(label2.Text);
+            for (int i = 0; true; i++)
+            {
+                int newPower = (int)Math.Pow(2, i);
+                if (newPower > currentPower)
+                {
+                    label2.Text = newPower.ToString();
+                    Thread.Sleep(1000);                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(t =>
+            {
+                label1.Invoke(new Action(delegate 
+                {
+                    label1.Text = "1";
+                    var timer = new System.Threading.Timer(_ => CalculatePrimeNumbers(), null, 0, 1000);
+                }));               
+            });
+            thread1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread2 = new Thread(t =>
+            {
+                label1.Invoke(new Action(delegate
+                {
+                    label2.Text = "0";
+                    var timer = new System.Threading.Timer(_ => CalculatePowersOfTwo(), null, 0, 1000);
+                }));
+            });
+            thread2.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Thread thread3 = new Thread(t =>
+            {
+                label3.Invoke(new Action(delegate
+                {
+                    label3.Text = "0";
+                    var timer = new System.Threading.Timer(_ => ShowUnixTime(), null, 0, 1000);
+                }));
+            });
+            thread3.Start();
+        }
+
+        private void ShowUnixTime()
+        {
+            while (true)
+            {
+                String unixTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                label3.Text = unixTimestamp;
+            }
+        }
+    }    
+}
